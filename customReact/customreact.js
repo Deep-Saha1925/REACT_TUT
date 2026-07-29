25f3231c402823b1434f27b8f6f5dcbbbad0b164
Deep-Saha1925 <dip23447@gmail.com>
2026-07-29 11:13:40 +0530

    Custom Render method

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,3 +1,11 @@
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+}
+
 const reactElement = {
     type: 'a',
     props: {
@@ -9,3 +17,4 @@ const reactElement = {
 
 const mainContainer = document.getElementById("root")
 
+customRender(reactElement, mainContainer)
