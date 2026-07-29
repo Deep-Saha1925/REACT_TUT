1b8293e344ddbee87c9414a3ec9a47ef9ccfee48
Deep-Saha1925 <dip23447@gmail.com>
2026-07-29 11:39:34 +0530

    Simple counter app

---
+++ counter/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  
+
+  return (
+    <>
+      <h1>React</h1>
+      <h3>Counter value: 5</h3>
+
+      <button>Increase</button>
+      <br />
+      <button>Decrease</button>
+    </>
+  )
+}
+
+export default App
