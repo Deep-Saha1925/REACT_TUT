908fa51ddadc1105b681787964e05e5a7e46e21c
Deep-Saha1925 <dip23447@gmail.com>
2026-07-29 11:19:41 +0530

    Updated function

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -11,7 +11,13 @@ function customRender(reactElement, container){
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children
 
-    
+    for(const prop in reactElement.props){
+        if(prop === 'children') continue;
+
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+
+    container.appendChild(domElement);
 }
 
 const reactElement = {
