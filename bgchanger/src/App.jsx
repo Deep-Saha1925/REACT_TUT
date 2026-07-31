65b2cdecd2753b50b11979329227e17524f35675
Deep-Saha1925 <dip23447@gmail.com>
2026-07-31 21:15:34 +0530

    Color injector

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -3,13 +3,14 @@ import { useState } from "react"
 
 function App() {
 
-  const [color, setColor] = useState("black");
-  
+  const [color, setColor] = useState("olive");
 
   return (
-    <>
+    <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}
+    >
       
-    </>
+    </div>
   )
 }
 
