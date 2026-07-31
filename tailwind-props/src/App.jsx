453e72bed9b79ca418d7e3136a26bcb77ff257a6
Deep-Saha1925 <dip23447@gmail.com>
2026-07-31 19:35:31 +0530

    Modified

--- tailwind-props/src/App.jsx
+++ tailwind-props/src/App.jsx
@@ -12,21 +12,6 @@ function App() {
     
       <h1 className='bg-green-400 text-black p-4 rounded-xl center'>Tailwind test</h1>
 
-          
-    <div class="flex flex-col items-center p-7 rounded-2xl">
-      <div>
-        <img class="size-48 shadow-xl rounded-md" alt="" src="https://www.pexels.com/photo/springtime-blossoms-in-norway-s-urban-setting-37363298/" />
-      </div>
-      <div class="flex">
-        <span class="text-2xl font-medium">Class Warfare</span>
-        <span>The Anti-Patterns</span>
-        <span class="flex">
-          <span>No. 4</span>
-          <span>·</span>
-          <span>2025</span>
-        </span>
-      </div>
-    </div>
     </>
   )
 }
