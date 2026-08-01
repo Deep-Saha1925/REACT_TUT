ef660d989db2c31cff3368e991b862be8fed1c23
Deep-Saha1925 <dip23447@gmail.com>
2026-08-01 14:04:03 +0530

    useCallback()

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
@@ -7,9 +7,9 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("")
 
-  const passwordGenerator = () => {
+  const passwordGenerator = useCallback(() => {
     
-  }
+  }, [length, numberAllowed, charAllowed, setPassword]) 
 
 
   return (
