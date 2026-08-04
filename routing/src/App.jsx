9edb9f9f884b497e4d0ae60c13d1cd30dc92ba76
Deep-Saha1925 <dip23447@gmail.com>
2026-08-04 12:27:32 +0530

    React routing.

---
+++ routing/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header/header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
