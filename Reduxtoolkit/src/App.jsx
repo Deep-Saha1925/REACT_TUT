6d4bea57fd1ff46cf01c09c513da6e695051f7bd
Deep-Saha1925 <dip23447@gmail.com>
2026-08-10 15:49:50 +0530

    Redux toolkit.

--- Reduxtoolkit/src/App.jsx
+++ Reduxtoolkit/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,8 @@ function App() {
     <>
     
     <h1>Redux toolkit</h1>
-    
+    <AddTodo />
+    <Todos />
     </>
   )
 }
