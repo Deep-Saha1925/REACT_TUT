028e4fde8f61af4f1b4d1188e218622e15366db8
Deep-Saha1925 <dip23447@gmail.com>
2026-08-11 21:04:43 +0530

    Mega blog.

---
+++ mega-blog/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import conf from './config/conf'
+
+function App() {
+
+  console.log(conf.appwriteUrl)
+
+  return (
+    <> 
+      <h1>Blog app</h1>
+    </>
+  )
+}
+
+export default App
