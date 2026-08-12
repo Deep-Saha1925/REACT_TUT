614d3a7855e94189239769fa9984d1ba5eb43c7a
Deep-Saha1925 <dip23447@gmail.com>
2026-08-12 20:38:37 +0530

    Mega blog.

--- mega-blog/src/appwrite/auth.js
+++ mega-blog/src/appwrite/auth.js
@@ -1,2 +1,14 @@
 import conf from '../config/conf.js'
 import { Client, Account, ID } from "appwrite";
+
+
+export class AuthSerive {
+    client = new Client();
+    account;
+
+    
+}
+
+const authService = new AuthSerive();
+
+export default authService
