39f6db80f8652b4bc1b78a06c621ebe3efe8ce45
Deep-Saha1925 <dip23447@gmail.com>
2026-08-12 21:48:13 +0530

    Mega blog.

--- mega-blog/src/appwrite/config.js
+++ mega-blog/src/appwrite/config.js
@@ -1,2 +1,20 @@
 import conf from '../config/conf.js'
-import { Client, Account, ID } from "appwrite";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+    client= new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+            this.databases = new Databases(this.client);
+            this.bucket = new Storage(this.client);
+    }
+}
+
+const service = new Service()
+
+export default service
