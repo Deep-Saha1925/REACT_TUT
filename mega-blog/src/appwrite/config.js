895832ec4aa92e506082d86c0d6c75afa1da5222
Deep-Saha1925 <dip23447@gmail.com>
2026-08-13 21:23:07 +0530

    config file

--- mega-blog/src/appwrite/config.js
+++ mega-blog/src/appwrite/config.js
@@ -13,6 +13,27 @@ export class Service {
             this.databases = new Databases(this.client);
             this.bucket = new Storage(this.client);
     }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectiontId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+
 }
 
 const service = new Service()
