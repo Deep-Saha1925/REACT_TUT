b32f7382dca4129faf8a5898447eab62f3142005
Deep-Saha1925 <dip23447@gmail.com>
2026-08-15 07:40:29 +0530

    Header jsz

--- mega-blog/src/components/Header/Header.jsx
+++ mega-blog/src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-import {Container, Logo, Logout} from '../index'
+import {Container, Logo, LogoutBtn} from '../index'
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 
 export default function Header(){
+
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+
+    
+
     return (
         <div>Header</div>
     )
