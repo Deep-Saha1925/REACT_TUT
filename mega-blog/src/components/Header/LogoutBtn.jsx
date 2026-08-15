633e5014b2a59fde6da095d1cec38fa5e97c400f
Deep-Saha1925 <dip23447@gmail.com>
2026-08-15 07:35:04 +0530

    Logout btn

--- mega-blog/src/components/Header/LogoutBtn.jsx
+++ mega-blog/src/components/Header/LogoutBtn.jsx
@@ -14,7 +14,7 @@ export default function LogoutBtn({ onLogout }) {
     }
 
   return (
-    <button onClick={logoutHandler} className="logout-btn">
+    <button onClick={logoutHandler} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
       Logout
     </button>
   );
