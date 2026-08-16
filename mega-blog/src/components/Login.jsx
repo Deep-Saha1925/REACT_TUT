ff6bde94e0162539c8bed7220853ffb18f500e38
Deep-Saha1925 <dip23447@gmail.com>
2026-08-16 20:30:08 +0530

    login using authservice

--- mega-blog/src/components/Login.jsx
+++ mega-blog/src/components/Login.jsx
@@ -4,9 +4,24 @@ import { login as authLogin } from '../store/authSlice'
 import {Button, Input, Logo} from './index'
 import { useDispatch } from "react-redux";
 import authService from '../appwrite/auth'
+import {useForm} from 'react-hook-form'
 
 function Login() {
-    
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("")
+
+    const login = async(data) => {
+        setError("")
+
+        try {
+            const session = await authService.login(data)
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+
 }
 
 export default Login
