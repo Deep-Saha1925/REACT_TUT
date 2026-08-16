cd0eb0a90690e83e74fa08f792a07275ac8a3660
Deep-Saha1925 <dip23447@gmail.com>
2026-08-16 20:17:23 +0530

    Post card

--- mega-blog/src/components/PostCard.jsx
+++ mega-blog/src/components/PostCard.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import appwriteService from '../appwrite/config'
+import {Link} from 'react-router-dom'
 
-function PostCard() {
+function PostCard({
+    $id, title, featuredImage
+}) {
 
 }
 
