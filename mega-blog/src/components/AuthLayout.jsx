c3d6b494c0a68489dbfad79a26a65e3c5f80c97d
Deep-Saha1925 <dip23447@gmail.com>
2026-08-18 15:48:53 +0530

    update authlayout

--- mega-blog/src/components/AuthLayout.jsx
+++ mega-blog/src/components/AuthLayout.jsx
@@ -2,4 +2,15 @@ import React, {useState, useEffct} from 'react'
 import {useSelector} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 
+export default function Protected({children, authentication = true}) {
 
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector((state) => state.auth.status)
+
+     [authStatus, navigate, authentication])
+    
+  return (
+    loader ? <div>Loading...</div> : children
+  )
+}
