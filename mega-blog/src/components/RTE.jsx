e20daf4c88192916dc08095010c6b36dcd14f1b5
Deep-Saha1925 <dip23447@gmail.com>
2026-08-19 07:13:39 +0530

    Default Editor

--- mega-blog/src/components/RTE.jsx
+++ mega-blog/src/components/RTE.jsx
@@ -5,7 +5,20 @@ export default function RTE() {
     <Editor
         initialValue="<p>This is the initial content of the editor.</p>"
         init={
-            branding: false
+            {
+                branding: false,
+                height: 500,
+                menubar: true,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                ],
+                toolbar: 'undo redo | formatselect | ' +
+                    'bold italic backcolor | alignleft aligncenter ' +
+                    'alignright alignjustify | bullist numlist outdent indent | ' +
+                    'removeformat | help',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            }
         }
     />
   )
