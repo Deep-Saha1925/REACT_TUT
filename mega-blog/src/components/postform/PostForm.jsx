7cc24e384b531d1ceb992ed673b0ae0aae1ec650
Deep-Saha1925 <dip23447@gmail.com>
2026-08-19 07:27:56 +0530

    Imported file

--- mega-blog/src/components/postform/PostForm.jsx
+++ mega-blog/src/components/postform/PostForm.jsx
@@ -1,8 +1,14 @@
 import React, {useCallback} from 'react'
 import {useForm, Controller} from 'react-hook-form'
 import {Button, Input, Select, RTE} from '../index'
+import appwriteService from "../../appwrite/config"
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 function PostForm() {
+
+    const {register, handleSubmit, watch} = useForm()
+
   return (
     <div>PostForm</div>
   )
