e13eda97e54b28c14f893fe267f08272d225bad4
Deep-Saha1925 <dip23447@gmail.com>
2026-08-19 07:17:03 +0530

    Controller from react hook

--- mega-blog/src/components/RTE.jsx
+++ mega-blog/src/components/RTE.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import {Editor} from '@tinymce/tinymce-react'
-export default function RTE() {
+import {Controller} from 'react-hook-form'
+
+
+export default function RTE({name, control,  label, defaultValue=""}) {
   return (
-    <Editor
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={
-            {
-                branding: false,
-                height: 500,
-                menubar: true,
-                plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                ],
-                toolbar: 'undo redo | formatselect | ' +
-                    'bold italic backcolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
-                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-            }
-        }
-    />
+    <div>test</div>
   )
 }
