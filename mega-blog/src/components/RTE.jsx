1a357076636681f4f847bf30a13d95978907299a
Deep-Saha1925 <dip23447@gmail.com>
2026-08-19 07:20:31 +0530

    Adding controller

--- mega-blog/src/components/RTE.jsx
+++ mega-blog/src/components/RTE.jsx
@@ -5,6 +5,17 @@ import {Controller} from 'react-hook-form'
 
 export default function RTE({name, control,  label, defaultValue=""}) {
   return (
-    <div>test</div>
+    <div className='w-full'>
+        {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+        <Controller
+            name={name || 'content'}
+            control={control}
+            render={({field: {onchange}}) => (
+                
+            )}
+        
+        />
+    </div>
   )
 }
