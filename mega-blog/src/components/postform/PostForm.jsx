5342b50610863324b7f9cdf7ed308bca231a9ce7
Deep-Saha1925 <dip23447@gmail.com>
2026-08-19 07:30:53 +0530

    Added default values from useform

--- mega-blog/src/components/postform/PostForm.jsx
+++ mega-blog/src/components/postform/PostForm.jsx
@@ -5,9 +5,16 @@ import appwriteService from "../../appwrite/config"
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 
-function PostForm() {
+function PostForm({post}) {
 
-    const {register, handleSubmit, watch} = useForm()
+    const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
+        defaultValues: {
+            title: post?.title || "",
+            slug: post?.slug || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        }
+    })
 
   return (
     <div>PostForm</div>
