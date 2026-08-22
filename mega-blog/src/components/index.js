4f4127fc058c73281c84b6d8b55e24fb6ee23a50
Deep-Saha1925 <dip23447@gmail.com>
2026-08-22 09:21:37 +0530

    Added in index file

--- mega-blog/src/components/index.js
+++ mega-blog/src/components/index.js
@@ -4,6 +4,7 @@ import Container from "./container/Container"
 import Logo from "./Logo"
 import Logout from "./Header/LogoutBtn"
 import RTE from "../components/RTE"
+import Signup from "../pages/Signup"
 
 export {
     Header,
@@ -11,5 +12,6 @@ export {
     Container,
     Logo,
     Logout,
-    RTE
+    RTE,
+    Signup
 }
