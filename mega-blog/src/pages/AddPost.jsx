d31b153c6ff1c5af778a215ab1dadd8838cf3f8c
Deep-Saha1925 <dip23447@gmail.com>
2026-08-22 09:30:07 +0530

    Add post

--- mega-blog/src/pages/AddPost.jsx
+++ mega-blog/src/pages/AddPost.jsx
@@ -3,7 +3,11 @@ import {Container, PostForm} from '../components'
 
 function AddPost() {
   return (
-    <div>AddPost</div>
+    <div className="py-8">
+      <Container>
+        <PostForm />
+      </Container>
+    </div>
   )
 }
 
