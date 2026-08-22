b34b0fd4ede4d226cc63f0940943eeed1cfc0b5e
Deep-Saha1925 <dip23447@gmail.com>
2026-08-22 09:35:56 +0530

    All posts

--- mega-blog/src/pages/AllPosts.jsx
+++ mega-blog/src/pages/AllPosts.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
-import appwriteService from '../services/appwriteServic'
+import React, {useState, useEffect} from 'react'
+import {Container, PostCard} from '../components'
+import appwriteService from '../appwrite/config'
+
 function AllPosts() {
+
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+    }, [])
+
+    appwriteService.getPosts([]).then((posts) => {
+        if(posts){
+            setPosts(posts.documents)
+        }
+    })
+
   return (
     <div>AllPosts</div>
   )
